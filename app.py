cea007c1998bf1aea7f3cf0de38b8f33154a9339
praTeek271 <prateekpatriotasme@gmail.com>
2023-04-30 00:10:08 +0530

    yyt

--- app.py
+++ app.py
@@ -1,15 +1,33 @@
 from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 from flask import send_from_directory
+from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
 import os
 
 # simple flask app 
 app = Flask(__name__)
+# databse connetion
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.sqlite"
+# initialize the app with the extension
+app.config['SECERET_KEY']=""
+db = SQLAlchemy(app)
+
+# user class
+class User(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String(50), nullable=False)
+    username=db.Column(db.String(50), nullable=False)
+    email=db.Column(db.String(20), nullable=False)
+    password=db.Column(db.String(20), nullable=False)
+    login=db.Column(db.Boolean, nullable=False)
+
+    def __repr__(self):
+        return(f'{self.name} | {self.username} | {self.email} | {self.login}')
+
 #upload_page
 UPLOAD_FOLDER = "UPLOAD_FOLDER"
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg'}
 @app.route('/upload', methods=['GET', 'POST'])
-
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
@@ -30,7 +48,6 @@ def upload_file():
 def allowed_file(filename):
     return ('.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS)
-
 @app.route('/uploads/<name>')
 def download_file(name):
     return (send_from_directory(app.config["UPLOAD_FOLDER"], name))
@@ -40,8 +57,17 @@ def download_file(name):
 def index():
     return(render_template('index.html'))
 
-@app.route('/user/login')
+@app.route('/user/login', methods=['POST','GET'])
 def signup():
+    if request.methods=='POST':
+        name=request.form['name']
+        username=request.form['username']
+        email=request.form['email']
+        password=request.form['password']
+        login=request.form['login']
+
+    
+
     return (render_template('user/user_log_signup.html',title="User1"))
 
 @app.route('/admin')
